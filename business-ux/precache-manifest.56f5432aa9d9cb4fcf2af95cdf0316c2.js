06944ad3e761ba6228e5434ce41a1d81df4dab02
Kenny Kenn <krecoding@gmail.com>
2019-06-19 17:51:52 -0400

    upload business-ux

---
+++ business-ux/precache-manifest.56f5432aa9d9cb4fcf2af95cdf0316c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0a071829890402f1094995a9105e84",
+    "url": "/business-ux/index.html"
+  },
+  {
+    "revision": "58094ec17a8646d54e27",
+    "url": "/business-ux/static/css/main.f36fcfe2.chunk.css"
+  },
+  {
+    "revision": "03ce91257493b2e21903",
+    "url": "/business-ux/static/js/2.8bb91f4b.chunk.js"
+  },
+  {
+    "revision": "58094ec17a8646d54e27",
+    "url": "/business-ux/static/js/main.bdafd511.chunk.js"
+  },
+  {
+    "revision": "3a97877a9bc3049dc618",
+    "url": "/business-ux/static/js/runtime~main.b21d5f5d.js"
+  }
+]);
