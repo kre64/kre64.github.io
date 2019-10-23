77e8338f23fc4c4ab72e9fdccb793635bfcac59b
Kenny Kenn <krecoding@gmail.com>
2019-10-22 22:23:29 -0400

    add files

---
+++ twitter-reviews/precache-manifest.d4f4ab4c69bb1dd751a209984ead715a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a525b285ae1b822aa30a70ad6361a81a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1e455cfaf6246d7fbad",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "285b8b780265184d5b82",
+    "url": "/static/css/main.02681868.chunk.css"
+  },
+  {
+    "revision": "a1e455cfaf6246d7fbad",
+    "url": "/static/js/2.6bbc8698.chunk.js"
+  },
+  {
+    "revision": "285b8b780265184d5b82",
+    "url": "/static/js/main.e67f6214.chunk.js"
+  },
+  {
+    "revision": "b532e38d1882a06cccfe",
+    "url": "/static/js/runtime-main.edc4bf8c.js"
+  },
+  {
+    "revision": "d00ce21957e1bc4c45f9e3ca57c14826",
+    "url": "/static/media/twitter_logo.d00ce219.svg"
+  }
+]);
