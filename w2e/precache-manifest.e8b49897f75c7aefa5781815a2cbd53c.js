fef6b95042e477af4a5e2dadf3b394eccff2de74
Kenny Kenn <krecoding@gmail.com>
2019-10-22 22:08:47 -0400

    add project via upload

---
+++ w2e/precache-manifest.e8b49897f75c7aefa5781815a2cbd53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b375543b630470ac66ffbf7cad30e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0c12eda3bef08178edf",
+    "url": "/static/css/2.c82fde7f.chunk.css"
+  },
+  {
+    "revision": "a0132c4b4201cc5f8d95",
+    "url": "/static/css/main.edce1302.chunk.css"
+  },
+  {
+    "revision": "d0c12eda3bef08178edf",
+    "url": "/static/js/2.d01345c7.chunk.js"
+  },
+  {
+    "revision": "a0132c4b4201cc5f8d95",
+    "url": "/static/js/main.f10311b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
