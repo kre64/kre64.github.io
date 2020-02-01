8bfaf754ad82ca9c70602dc8ff1c7e57304a54a1
kevin <krecoding@gmail.com>
2020-01-31 23:19:59 -0500

    v2

---
+++ survive/precache-manifest.e96accb91f79fd86abb14af943bd755d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec15d417bb866808ba52b5f81df7917",
+    "url": "/survive/index.html"
+  },
+  {
+    "revision": "1102c0dd38f6e2a8c29b",
+    "url": "/survive/static/css/main.65f61b91.chunk.css"
+  },
+  {
+    "revision": "c71e62d467af074cd3fd",
+    "url": "/survive/static/js/2.23661145.chunk.js"
+  },
+  {
+    "revision": "7c4ada3a920bd5841bb63ce5c4cc22a3",
+    "url": "/survive/static/js/2.23661145.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1102c0dd38f6e2a8c29b",
+    "url": "/survive/static/js/main.cc514f0b.chunk.js"
+  },
+  {
+    "revision": "513eb0f1ff7f58e5ff35",
+    "url": "/survive/static/js/runtime-main.00639b3d.js"
+  }
+]);
--- survive/service-worker.js
+++ survive/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91ab3a27b5a3f8b81a97f08520511203.js"
+  "/survive/precache-manifest.e96accb91f79fd86abb14af943bd755d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/survive/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
